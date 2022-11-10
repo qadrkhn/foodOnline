1bb7671ca74754597fdbad12bed31da25ba28db1
Qadeer Khan <qadeerkhan@ucp.edu.pk>
2022-11-10 22:47:19 +0500

    resgister user added and front page setup

---
+++ accounts/views.py
@@ -0,0 +1,36 @@
+from http.client import HTTPResponse
+from django.shortcuts import render,redirect
+from django.shortcuts import HttpResponse
+from .forms import UserForm
+from .models import User
+from django.contrib import messages
+
+# Create your views here.
+def registerUser(request):
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            # password = form.cleaned_data['password']
+            # user = form.save(commit = False)
+            # user.set_password(password)
+            # user.role = User.CUSTOMER
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user = User.objects.create_user(first_name=first_name,last_name=last_name,username=username,email=email,password=password)
+            user.role = User.CUSTOMER
+            user.save()
+            messages.success(request,'Your Account Has Been Created Successfully')
+            return redirect('registerUser')
+        else:
+            if form.errors:
+                print(form.errors)
+    else:
+        form = UserForm()
+    
+    context = {
+        'form':form,
+    }
+    return render(request,'accounts/registerUser.html',context)
