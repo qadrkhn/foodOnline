b651b442b8c3dd19587e35b19464d9a718e7e6c2
Qadeer Khan <qadeerkhan@ucp.edu.pk>
2022-11-12 18:22:40 +0500

    Added Dashboard for Customer and Vendor

--- accounts/views.py
+++ accounts/views.py
@@ -3,12 +3,36 @@ from django.shortcuts import render,redirect
 from django.shortcuts import HttpResponse
 from .forms import UserForm
 from .models import User,UserProfile
-from django.contrib import messages
+from django.contrib import messages,auth
 from vendor.forms import VendorForm
+from .utils import detectUser
+from django.contrib.auth.decorators import login_required,user_passes_test
+from django.core.exceptions import PermissionDenied
+
+
+#Custom decorators for restricting access to dashbaords vendor cannot access customer dashboard vice versa.
+
+def is_vendor(user):
+    if user.role == 1:
+        return True
+    else:
+        raise PermissionDenied
+
+def is_customer(user):
+    if user.role == 2:
+        return True
+    else:
+        raise PermissionDenied
+
+
+
 
 # Create your views here.
 def registerUser(request):
-    if request.method == 'POST':
+    if request.user.is_authenticated:
+        messages.warning(request,'You Are Already Logged In! ')
+        return redirect('myAccount')
+    elif request.method == 'POST':
         form = UserForm(request.POST)
         if form.is_valid():
             # password = form.cleaned_data['password']
@@ -37,7 +61,10 @@ def registerUser(request):
     return render(request,'accounts/registerUser.html',context)
 
 def registerVendor(request):
-    if request.method == 'POST':
+    if request.user.is_authenticated:
+        messages.warning(request,'You Are Already Logged In! ')
+        return redirect('myAccount')
+    elif request.method == 'POST':
         form = UserForm(request.POST)
         v_form = VendorForm(request.POST,request.FILES)
         if form.is_valid() and v_form.is_valid():
@@ -69,3 +96,49 @@ def registerVendor(request):
     }
 
     return render(request,'accounts/registerVendor.html',context)
+
+
+def login(request):
+    if request.user.is_authenticated:
+        messages.warning(request,'You Are Already Logged In! ')
+        return redirect('myAccount')
+    elif request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+
+        user = auth.authenticate(email=email,password=password)
+
+        if user is not None:
+            auth.login(request,user)
+            messages.success(request,' You Are Now Logged In ')
+            return redirect('myAccount')
+        else:
+            messages.error(request,' Invalid Login Details. Please Enter Correct Email And Password ')
+            return redirect('login')
+
+
+
+    return render(request,'accounts/login.html')
+
+def logout(request):
+    auth.logout(request)
+    messages.info(request,' You Are Now Logged Out. ')
+    return redirect('login')
+
+
+@login_required(login_url='login')
+@user_passes_test(is_customer)
+def custDashboard(request):
+    return render(request,'accounts/custDashboard.html')
+
+@login_required(login_url='login')
+@user_passes_test(is_vendor)
+def vendorDashboard(request):
+    return render(request,'accounts/vendorDashboard.html')
+
+@login_required(login_url='login')
+def myAccount(request):
+    user = request.user
+    redirectUrl = detectUser(user)
+    return redirect(redirectUrl)
+
